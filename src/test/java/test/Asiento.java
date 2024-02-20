3d349d3b23df85b492417cbc9e6e4dfc073e69fd
seestradav <seestradav@unal.edu.co>
2024-02-19 22:41:54 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,13 +1,12 @@
 package test;
-public class Asiento{
-String color;
-int precio;
-int registro;
-void cambiarColor (String color){	
-string[] lista={"rojo","amarillo","verde","blanco","negro"};
-for (int i=0;i<lista.length; i++){
-	if (lista[i]==color)
-	Asientos.color=color;
-	break;}
-}
+public class Asiento {
+	String color;
+	int precio;
+	int registro;
+	void cambiarColor (String color){	
+	string[] lista={"rojo","amarillo","verde","blanco","negro"};
+	for (int i=0;i<lista.length; i++){
+		if (lista[i]==color)
+		Asientos.color=color;
+		break;}
 }
