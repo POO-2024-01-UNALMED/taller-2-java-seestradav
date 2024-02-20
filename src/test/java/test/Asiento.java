4c7ff82cb9fea206f652421be104c82caeaea4e9
seestradav <seestradav@unal.edu.co>
2024-02-20 10:19:56 -0500

    x

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,21 +1,26 @@
 package test;
+
 public class Asiento {
 	String color;
 	int precio;
 	int registro;
-   //void cambiarColor(String color) {
-        //String[] valoresPermitidos = {"rojo", "verde", "amarillo", "negro", "blanco"};
-        //boolean esColorPermitido = false;
+	
+	void cambiarColor(String color) {
+		if (color == "rojo") {
+			this.color = color
+		}
+		if (color == "verde") {
+			this.color = color
+		}
+		if (color == "amarillo") {
+			this.color = color
+		}
+		if (color == "negro") {
+			this.color = color
+		}
+		if (color == "blanco") {
+			this.color = color
+		}
+	}
 
-        //for (String valor : valoresPermitidos) {
-            //if (color.equals(valor)) {
-                //esColorPermitido = true;
-                //break;
-            //}
-        //}
-
-        //if (esColorPermitido) {
-            //this.color = color;
-        //}
-    //}
 }
