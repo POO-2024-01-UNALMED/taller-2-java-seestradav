331c3dab1d68d3cc170d9b9c54a4b4140982111d
seestradav <seestradav@unal.edu.co>
2024-02-20 10:34:32 -0500

    j

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,17 @@
+package test;
+public class Auto {
+	Static int cantidadCreados;
+	String modelo;
+	int precio;
+	Asiento[]asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	
+	//int cantidadAsientos() {
+		//int = 0;
+		//for(Asiento a: this.asientos) {
+			i
+		//}
+	}
+}
