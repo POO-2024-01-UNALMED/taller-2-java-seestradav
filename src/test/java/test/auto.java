eb0685e4efb775d8a9f1f0c044fa760b4887d476
seestradav <seestradav@unal.edu.co>
2024-02-19 22:46:17 -0500

    cambios 18

--- src/test/java/test/auto.java
+++ src/test/java/test/auto.java
@@ -1,23 +1,23 @@
 package test;
-public class Auto{
-String modelo;
-int precio;
-Asiento[] asientos;
-String marca;
-Motor motor;
-int registro;
-static int cantidadcreados;
-int cantidadAsientos(Asiento[] asientos){
-	n=0;
-	for (int i=0;i<asientos.length; i++){
-		if (asientos[i] instanceof Asiento)
-		n++;}
-	return n;
-}
-String verificarIntegridad(int registro) {
-	if (registro==Asiento.registro && registro==Motor.registro)
-		System.out.println("Auto original");	
-	else 
-		System.out.println("las piezas no son originales");
-}
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadcreados;
+	int cantidadAsientos(Asiento[] asientos){
+		n=0;
+		for (int i=0;i<asientos.length; i++){
+			if (asientos[i] instanceof Asiento)
+			n++;}
+		return n;
+	}
+	String verificarIntegridad(int registro) {
+		if (registro==Asiento.registro && registro==Motor.registro)
+			System.out.println("Auto original");	
+		else 
+			System.out.println("las piezas no son originales");
+	}
 }
--- src/test/java/test/motor.java
+++ src/test/java/test/motor.java
@@ -1,18 +1,17 @@
-
 package test;
-public class Motor{
-int numeroCilindros;
-String tipo;
-int registro;
-void asignarTipo(String tipo) {
-	if(tipo=="gasolina") 
-			Motor.tipo=tipo;
-	else if(tipo=="electrico")
-			Motor.tipo=tipo;
-	else 
-		Motor.tipo= null;
-}
-void cambiarRegistro(int registro) {
-	Motor.registro=registro;
-}
+public class Motor {
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	void asignarTipo(String tipo) {
+		if(tipo=="gasolina") 
+				Motor.tipo=tipo;
+		else if(tipo=="electrico")
+				Motor.tipo=tipo;
+		else 
+			Motor.tipo= null;
+	}
+	void cambiarRegistro(int registro) {
+		Motor.registro=registro;
+	}
 }
