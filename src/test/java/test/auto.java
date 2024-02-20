5bf1101affc55c66124e7ba12ce04c0732cdda07
seestradav <seestradav@unal.edu.co>
2024-02-19 22:21:38 -0500

    Update auto.java

--- src/test/java/test/auto.java
+++ src/test/java/test/auto.java
@@ -2,20 +2,20 @@ package test;
 public class auto {
 String modelo;
 int precio;
-Asientos[] asientos;
+Asiento[] asientos;
 String marca;
 motor motor;
 int registro;
 static int cantidadcreados;
-int cantidadAsientos(Asientos[] asientos){
+int cantidadAsientos(Asiento[] asientos){
 	n=0;
 	for (int i=0;i<asientos.length; i++){
-		if (asientos[i] instanceof Asientos)
+		if (asientos[i] instanceof Asiento)
 		n++;}
 	return n;
 }
 String verificarIntegridad(int registro) {
-	if (registro==Asientos.registro && registro==motor.registro)
+	if (registro==Asiento.registro && registro==motor.registro)
 		System.out.println("Auto original");	
 	else 
 		System.out.println("las piezas no son originales");
